f297f4b1210646f8fbb43576bff14dc3d3818caa
Luis Gómez <luisgaleano002@gmail.com>
2020-09-26 12:40:11 -0500

    Cambio de función por defecto para las fechas

--- models/product.go
+++ models/product.go
@@ -7,6 +7,6 @@ type Product struct {
 	Name      string    `pg:"type:varchar(60)" json:"name"`
 	UserID    uint32    `pg:"on_delete:CASCADE,unique" json:"user_id"`
 	User      *User     `pg:"rel:has-one" json:"user,omitempty"`
-	CreatedAt time.Time `pg:"default:CURRENT_TIMESTAMP" json:"created_at"`
-	UpdatedAt time.Time `pg:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+	CreatedAt time.Time `pg:"default:now()" json:"created_at"`
+	UpdatedAt time.Time `pg:"default:now()" json:"updated_at"`
 }
--- models/user.go
+++ models/user.go
@@ -13,8 +13,8 @@ type User struct {
 	LastName  string    `pg:"type:varchar(40)" json:"last_name"`
 	Email     string    `pg:"type:varchar(60)" pg:",unique" json:"email"`
 	Password  string    `pg:"type:varchar(128)" json:"-"`
-	CreatedAt time.Time `pg:"default:CURRENT_TIMESTAMP" json:"created_at"`
-	UpdatedAt time.Time `pg:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+	CreatedAt time.Time `pg:"default:now()" json:"created_at"`
+	UpdatedAt time.Time `pg:"default:now()" json:"updated_at"`
 }
 
 var _ pg.BeforeInsertHook = (*User)(nil)
